08e27ea52fd49a5dc80a390b7a5bf4c07bf5a4f6
Muhammad Awais <175257405+poorman718@users.noreply.github.com>
2026-05-23 03:31:14 +0500

    Create config.js

---
+++ assets/js/config.js
@@ -0,0 +1,5 @@
+const CONFIG = {
+    API_KEY: 'YOUR_RAPIDAPI_KEY',   // 👈 Replace with your key
+    API_HOST: 'instagram-downloader-download-instagram-videos-stories1.p.rapidapi.com',
+    API_BASE_URL: 'https://instagram-downloader-download-instagram-videos-stories1.p.rapidapi.com/'
+};
