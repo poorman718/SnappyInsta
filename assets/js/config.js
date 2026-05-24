66d46eaeba5c8440aa85fd61611d834e094609bc
Muhammad Awais <175257405+poorman718@users.noreply.github.com>
2026-05-24 18:52:28 +0500

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    API_KEY: '23d7792dfamshac4ad4751a68f49p1438c8jsn17a353c04fbd',   // 👈 yahan apni key dalen
-    API_HOST: 'instagram-downloader-download-instagram-videos-stories1.p.rapidapi.com',
-    API_BASE_URL: 'https://instagram-downloader-download-instagram-videos-stories1.p.rapidapi.com/get-info-rapidapi'
+    API_KEY: '23d7792dfamshac4ad4751a68f49p1438c8jsn17a353c04fbd',
+    API_HOST: 'instagram-tiktok-youtube-downloader.p.rapidapi.com',
+    API_BASE_URL: 'https://instagram-tiktok-youtube-downloader.p.rapidapi.com/get-info'
 };
