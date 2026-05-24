c4dd1f11d413d0f6462fa83d333aedf4ca46fb11
Muhammad Awais <175257405+poorman718@users.noreply.github.com>
2026-05-24 18:57:07 +0500

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
     API_KEY: '23d7792dfamshac4ad4751a68f49p1438c8jsn17a353c04fbd',
-    API_HOST: 'instagram-tiktok-youtube-downloader.p.rapidapi.com',
-    API_BASE_URL: 'https://instagram-tiktok-youtube-downloader.p.rapidapi.com/get-info'
+    API_HOST: 'instagram-statistics-api.p.rapidapi.com',
+    API_BASE_URL: 'https://instagram-statistics-api.p.rapidapi.com/posts/one'
 };
